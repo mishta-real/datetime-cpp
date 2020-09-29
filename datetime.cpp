0722c75f858abb41bbbd9e310edba41a32248769
mishta-real <mikado.brave@gmail.com>
2020-09-29 16:47:34 +0700

    Added function to convert date to seconds since epoch and vice versa

--- datetime.cpp
+++ datetime.cpp
@@ -5,246 +5,31 @@
 #include <iomanip>
 #include <iostream>
 
-inline bool DateTime::isYearLeap()
+namespace DateTime {
+
+bool isYearLeap(const int t_year)
 {
-    return !(year % 4);
+    return
+        ((t_year % 4 == 0) && (t_year % 100 != 0)) ||
+        (t_year % 400 == 0);
 }
 
-inline bool isYearLeap(const int t_year)
+inline bool isDayCorrect(const int t_day, const int t_month, const int t_year)
 {
-    return !(t_year % 4);
-}
-
-void DateTime::getFull()
-{
-    // Print the current date & time
-    std::cout << std::setfill('0')
-        << std::setw(2) << day << '.'
-        << std::setw(2) << month << '.'
-        << std::setw(4) << year << ' '
-        << std::setw(2) << hour << ':'
-        << std::setw(2) << minute << ':'
-        << std::setw(2) << second;
-}
-
-void DateTime::getDate()
-{
-    // Print the current date
-    std::cout << std::setfill('0')
-        << std::setw(2) << day << '.'
-        << std::setw(2) << month << '.'
-        << std::setw(4) << year;
-}
-
-void DateTime::getTime()
-{
-    // Print the current time
-    std::cout << std::setfill('0')
-        << std::setw(2) << hour << ':'
-        << std::setw(2) << minute << ':'
-        << std::setw(2) << second;
-}
-
-// The next day will be assigned to dt
-void DateTime::getNextDay(DateTime &dt)
-{
-    int nextDay = day + 1;
-    int nextMonth = month;
-
-    if (nextDay > DAYS_IN_MONTH[month]) {
-        // Not February or not a leap year condition:
-        if (!isYearLeap() || month != 2) {
-            nextDay = 1;
-            ++nextMonth;
-        }
-        // February 29th case:
-        else {
-            // None
-        }
-    }
-
-    int nextYear = year;
-
-    if (nextMonth > MONTHS_IN_YEAR) {
-        nextMonth = 1;
-        ++nextYear;
-    }
-
-    dt = { nextDay, nextMonth, nextYear };
-}
-
-void DateTime::getNextDay()
-{
-    int nextDay = day + 1;
-    int nextMonth = month;
-
-    if (nextDay > DAYS_IN_MONTH[month]) {
-        // Not february or not a leap year condition:
-        if (!isYearLeap() || month != 2) {
-            nextDay = 1;
-            ++nextMonth;
-        }
-        // February 29th case:
-        else {
-            // None
-        }
-    }
-
-    int nextYear = year;
-
-    if (nextMonth > MONTHS_IN_YEAR) {
-        nextMonth = 1;
-        ++nextYear;
-    }
-
-    std::cout << std::setfill('0')
-        << std::setw(2) << nextDay << '.'
-        << std::setw(2) << nextMonth << '.'
-        << std::setw(4) << nextYear;
-}
-
-// The previous day will be assigned to dt
-void DateTime::getPrevDay(DateTime &dt)
-{
-    int prevDay = day;
-    int prevMonth = month;
-    int prevYear = year;
-
-    if (year != FIRST_YEAR || month != FIRST_MONTH || day != FIRST_DAY)
-    {
-        --prevDay;
-
-        if (prevDay > 0) {
-            // None
-        }
-        else {
-            --prevMonth;
-
-            if (isYearLeap() && prevMonth == 2) {
-                prevDay = LEAP_DAY;
-            }
-            else if (prevMonth >= FIRST_MONTH) {
-                prevDay = DAYS_IN_MONTH[prevMonth];
-            }
-            else {
-                --prevYear;
-                prevMonth = 12;
-                prevDay = DAYS_IN_MONTH[prevMonth];
-            }
-        }
+    if (!isYearLeap(t_year) || t_month != 2) {
+        return FIRST_DAY <= t_day && t_day <= DAYS_IN_MONTH[t_month];
     }
     else {
-        // The first day - do nothing
-    }
-
-    dt = { prevDay, prevMonth, prevYear };
-}
-
-void DateTime::getPrevDay()
-{
-    int prevDay = day;
-    int prevMonth = month;
-    int prevYear = year;
-
-    if (year != FIRST_YEAR || month != FIRST_MONTH || day != FIRST_DAY)
-    {
-        --prevDay;
-
-        if (prevDay > 0) {
-            // None
-        }
-        else {
-            --prevMonth;
-
-            if (isYearLeap() && prevMonth == 2) {
-                prevDay = LEAP_DAY;
-            }
-            else if (prevMonth >= FIRST_MONTH) {
-                prevDay = DAYS_IN_MONTH[prevMonth];
-            }
-            else {
-                --prevYear;
-                prevMonth = 12;
-                prevDay = DAYS_IN_MONTH[prevMonth];
-            }
-        }
-    }
-    else {
-        // The first day - do nothing
-    }
-
-    std::cout << std::setfill('0')
-        << std::setw(2) << prevDay << '.'
-        << std::setw(2) << prevMonth << '.'
-        << std::setw(4) << prevYear;
-}
-
-void DateTime::parseDate(const std::string &line)
-{
-    // The only format is "DD.MM.YYYY"
-
-    // Parsing the day
-    if (std::isdigit(line[0]) && std::isdigit(line[1])) {
-        int bufferDay = (line[0] - '0') * 10 + line[1] - '0';
-        if (isDayCorrect(bufferDay, month, year)) {
-            day = bufferDay;    // Adding day
-        }
-        else {
-            dayIncorrectError(bufferDay);
-        }
-    }
-    else {
-        dayIncorrectError(0);
-    }
-
-    // Parsing the month
-    if (std::isdigit(line[3]) && std::isdigit(line[4])) {
-        int bufferMonth = (line[3] - '0') * 10 + line[4] - '0';
-        if (isMonthCorrect(bufferMonth)) {
-            month = bufferMonth;    // Adding month
-        }
-        else {
-            monthIncorrectError(bufferMonth);
-        }
-    }
-    else {
-        monthIncorrectError(0);
-    }
-
-    // Parsing the year
-    if (std::isdigit(line[6]) && std::isdigit(line[7])
-        && std::isdigit(line[8]) && std::isdigit(line[9]))
-    {
-        int bufferYear = (line[6] - '0') * 1000
-            + (line[7] - '0') * 100
-            + (line[8] - '0') * 10
-            + line[9] - '0';
-        if (isYearCorrect(bufferYear)) {
-            year = bufferYear;  // Adding year
-        }
-        else {
-            yearIncorrectError(bufferYear);
-        }
-    }
-    else {
-        yearIncorrectError(0);
+        return FIRST_DAY <= t_day && t_day <= LEAP_DAY;
     }
 }
 
-bool isDayCorrect(const int t_day, const int t_month, const int t_year)
-{
-    return FIRST_DAY <= t_day && (t_day <= DAYS_IN_MONTH[t_month] ||
-         (isYearLeap(t_year) &&
-         t_month == 2 &&
-         t_day <= LEAP_DAY));
-}
-
-bool isMonthCorrect(const int t_month)
+inline bool isMonthCorrect(const int t_month)
 {
     return FIRST_MONTH <= t_month && t_month <= MONTHS_IN_YEAR;
 }
 
-bool isYearCorrect(const int t_year)
+inline bool isYearCorrect(const int t_year)
 {
     return FIRST_YEAR <= t_year;
 }
@@ -285,6 +70,221 @@ void yearIncorrectError(const int t_year=0)
     std::exit(1);
 }
 
+// Print date & time
+void DateTime::getDateTime() const
+{
+    std::cout <<
+        std::setfill('0') <<
+        std::setw(2) << day << '.' <<
+        std::setw(2) << month << '.' <<
+        std::setw(4) << year << ' ' <<
+        std::setw(2) << hour << ':' <<
+        std::setw(2) << minute << ':' <<
+        std::setw(2) << second;
+}
+
+// Print date
+void DateTime::getDate() const
+{
+    std::cout <<
+        std::setfill('0') <<
+        std::setw(2) << day << '.' <<
+        std::setw(2) << month << '.' <<
+        std::setw(4) << year;
+}
+
+// Print time
+void DateTime::getTime() const
+{
+    std::cout <<
+        std::setfill('0') <<
+        std::setw(2) << hour << ':' <<
+        std::setw(2) << minute << ':' <<
+        std::setw(2) << second;
+}
+
+void DateTime::getNextDay() const
+{
+    int nextDay = day + 1;
+    int nextMonth = month;
+
+    if (!isYearLeap(year)) {
+        if (nextDay > DAYS_IN_MONTH[month]) {
+            nextDay = 1;
+            ++nextMonth;
+        }
+    }
+    else {
+        if ((month != 2 && nextDay > DAYS_IN_MONTH[month]) ||
+            (month == 2 && nextDay > LEAP_DAY))
+        {
+            nextDay = 1;
+            ++nextMonth;
+        }
+    }
+
+    int nextYear = year;
+
+    if (nextMonth > MONTHS_IN_YEAR) {
+        nextMonth = 1;
+        ++nextYear;
+    }
+
+    std::cout <<
+        std::setfill('0') <<
+        std::setw(2) << nextDay << '.' <<
+        std::setw(2) << nextMonth << '.' <<
+        std::setw(4) << nextYear;
+}
+
+void DateTime::getPrevDay()
+{
+    int prevDay = day;
+    int prevMonth = month;
+    int prevYear = year;
+
+    // If date is not 01.01.1970:
+    if (year != FIRST_YEAR || month != FIRST_MONTH || day != FIRST_DAY) {
+        prevDay -= 1;
+
+        if (prevDay >= FIRST_DAY) {
+            // None
+        }
+        // If day is 0 we change the month and then the day and even the year:
+        else {
+            prevMonth -= 1;
+
+            if (isYearLeap(year) && prevMonth == 2) {
+                // Feb 29:
+                prevDay = LEAP_DAY;
+            }
+            else if (prevMonth >= FIRST_MONTH) {
+                prevDay = DAYS_IN_MONTH[prevMonth]; // Last day of the month
+            }
+            else {
+                prevYear -= 1;
+                // Dec 31:
+                prevMonth = 12;
+                prevDay = DAYS_IN_MONTH[prevMonth];
+            }
+        }
+    }
+
+    std::cout <<
+        std::setfill('0') <<
+        std::setw(2) << prevDay << '.' <<
+        std::setw(2) << prevMonth << '.' <<
+        std::setw(4) << prevYear;
+}
+
+TimeDelta::TimeDelta DateTime::operator-(const DateTime &datetime2) const
+{
+    long long dt1_seconds = dateToSeconds(*this);
+    long long dt2_seconds = dateToSeconds(datetime2);
+
+    long long timedelta_seconds = std::abs(dt1_seconds - dt2_seconds);
+
+    TimeDelta::TimeDelta td;
+    TimeDelta::secondsToTimeDelta(timedelta_seconds, td);
+    return td;
+}
+
+DateTime DateTime::operator+(const TimeDelta::TimeDelta &timedelta) const
+{
+    long long dt1_seconds = dateToSeconds(*this);
+    long long td_seconds = TimeDelta::timeDeltaToSeconds(timedelta);
+
+    long long dt2_seconds = dt1_seconds + td_seconds;
+
+    DateTime dt2;
+    secondsToDate(dt2_seconds, dt2);
+    return dt2;
+}
+
+DateTime DateTime::operator-(const TimeDelta::TimeDelta &timedelta)
+{
+    long long dt1_seconds = dateToSeconds(*this);
+    long long td_seconds = TimeDelta::timeDeltaToSeconds(timedelta);
+
+    long long dt2_seconds = dt1_seconds > td_seconds ? dt1_seconds - td_seconds : 0;
+
+    DateTime dt2;
+    if (dt2_seconds) {
+        secondsToDate(dt2_seconds, dt2);
+    }
+    else {
+        dt2 = {
+            FIRST_DAY, FIRST_MONTH, FIRST_YEAR,
+            FIRST_HOUR, FIRST_MINUTE, FIRST_SECOND
+        };
+    }
+    return dt2;
+}
+
+void parseDate(const std::string &dateString, DateTime &date)
+{
+    // The only format is "DD.MM.YYYY"
+
+    // Parsing the day
+    if (std::isdigit(dateString[0])
+        && std::isdigit(dateString[1]))
+    {
+        int bufferDay = (dateString[0] - '0') * 10
+            + (dateString[1] - '0');
+
+        if (isDayCorrect(bufferDay, date.month, date.year)) {
+            date.day = bufferDay;
+        }
+        else {
+            dayIncorrectError(bufferDay);
+        }
+    }
+    else {
+        dayIncorrectError(0);
+    }
+
+    // Parsing the month
+    if (std::isdigit(dateString[3])
+        && std::isdigit(dateString[4]))
+    {
+        int bufferMonth = (dateString[3] - '0') * 10
+            + (dateString[4] - '0');
+
+        if (isMonthCorrect(bufferMonth)) {
+            date.month = bufferMonth;
+        }
+        else {
+            monthIncorrectError(bufferMonth);
+        }
+    }
+    else {
+        monthIncorrectError(0);
+    }
+
+    // Parsing the year
+    if (std::isdigit(dateString[6])
+        && std::isdigit(dateString[7])
+        && std::isdigit(dateString[8])
+        && std::isdigit(dateString[9]))
+    {
+        int bufferYear = (dateString[6] - '0') * 1000
+            + (dateString[7] - '0') * 100
+            + (dateString[8] - '0') * 10
+            + (dateString[9] - '0');
+
+        if (isYearCorrect(bufferYear)) {
+            date.year = bufferYear;
+        }
+        else {
+            yearIncorrectError(bufferYear);
+        }
+    }
+    else {
+        yearIncorrectError(0);
+    }
+}
+
+
 DateTime systemTime()
 {
     time_t t = std::time(0);
@@ -301,3 +301,90 @@ DateTime systemTime()
 
     return sysTime;
 }
+
+// Get date since epoch in seconds
+long long int dateToSeconds(const DateTime &dt)
+{
+    long long int seconds = dt.second
+        + dt.minute * SECONDS_IN_MINUTE
+        + dt.hour * SECONDS_IN_HOUR
+        + (dt.day - 1) * SECONDS_IN_DAY;
+
+    for (int t_month = dt.month - 1; t_month >= FIRST_MONTH; --t_month) {
+        if (!isYearLeap(dt.year) || t_month != 2) {
+            seconds += DAYS_IN_MONTH[t_month] * SECONDS_IN_DAY;
+        }
+        else {
+            seconds += DAYS_IN_LEAP_MONTH * SECONDS_IN_DAY;
+        }
+    }
+
+    for (int t_year = dt.year - 1; t_year >= FIRST_YEAR; --t_year) {
+        if (!isYearLeap(t_year)) {
+            seconds += DAYS_IN_YEAR * SECONDS_IN_DAY;
+        }
+        else {
+            seconds += DAYS_IN_LEAP_YEAR * SECONDS_IN_DAY;
+        }
+    }
+
+    return seconds;
+}
+
+// Get date since epoch in normal format
+void secondsToDate(const long long seconds, DateTime &dt)
+{
+    const int time_seconds = seconds % SECONDS_IN_DAY;
+
+    dt.hour = time_seconds / SECONDS_IN_HOUR;
+    dt.minute = time_seconds / MINUTES_IN_HOUR % SECONDS_IN_MINUTE;
+    dt.second = time_seconds % SECONDS_IN_MINUTE;
+
+    int date_seconds = seconds - time_seconds;
+
+    int years = 0;
+    for (int y = years + FIRST_YEAR;
+        date_seconds > SECONDS_IN_YEAR;
+        ++y, ++years)
+    {
+        if (!isYearLeap(y)) {
+            date_seconds -= SECONDS_IN_YEAR;
+        }
+        else {
+            date_seconds -= SECONDS_IN_LEAP_YEAR;
+        }
+    }
+
+    dt.year = FIRST_YEAR + years;
+
+    int months = 0;
+    for (int m = 1;
+        date_seconds > DAYS_IN_MONTH[m] * SECONDS_IN_DAY;
+        ++m, ++months)
+    {
+        if (!isYearLeap(dt.year) || m != 2) {
+            date_seconds -= DAYS_IN_MONTH[m] * SECONDS_IN_DAY;
+        }
+        else {
+            date_seconds -= DAYS_IN_LEAP_MONTH * SECONDS_IN_DAY;
+        }
+    }
+
+    dt.month = months + 1;
+
+    const int days = date_seconds / SECONDS_IN_DAY + 1;
+
+    if (days <= DAYS_IN_MONTH[dt.month] || (isYearLeap(dt.year) && dt.month == 2)) {
+        dt.day = days;
+    }
+    else {
+        dt.day = FIRST_DAY;
+        ++dt.month;
+        if (dt.month > MONTHS_IN_YEAR) {
+            dt.month = FIRST_MONTH;
+            ++dt.year;
+        }
+    }
+}
+
+} // namespace DateTime
--- datetime.hpp
+++ datetime.hpp
@@ -1,74 +1,95 @@
 #ifndef DATETIME_HPP
 #define DATETIME_HPP
 
+#include "timedelta.hpp"
+
 #include <string>
 
-const int FIRST_YEAR = 1970;
-const int FIRST_MONTH = 1;
-const int FIRST_DAY = 1;
+constexpr int FIRST_YEAR = 1970;
+constexpr int FIRST_MONTH = 1;
+constexpr int FIRST_DAY = 1;
 
-const int FIRST_SECOND = 0;
-const int FIRST_MINUTE = 0;
-const int FIRST_HOUR = 0;
+constexpr int FIRST_SECOND = 0;
+constexpr int FIRST_MINUTE = 0;
+constexpr int FIRST_HOUR = 0;
 
-const int SECONDS_IN_MINUTE = 60;
-const int SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60;
-const int SECONDS_IN_DAY = SECONDS_IN_HOUR * 24;
+constexpr int MONTHS_IN_YEAR = 12;
 
-const int SECONDS_IN_28_DAYS = SECONDS_IN_DAY * 28;
-const int SECONDS_IN_29_DAYS = SECONDS_IN_DAY * 28;
-const int SECONDS_IN_30_DAYS = SECONDS_IN_DAY * 30;
-const int SECONDS_IN_31_DAYS = SECONDS_IN_DAY * 31;
-
-const int SECONDS_IN_YEAR = SECONDS_IN_DAY * 365.25;
-
-const int MONTHS_IN_YEAR = 12;
-
-const int DAYS_IN_MONTH[MONTHS_IN_YEAR + 1] = {
+constexpr int DAYS_IN_MONTH[MONTHS_IN_YEAR + 1] = {
     0,  // Needed to access Jan at index 1, not 0; Feb at index 2, not 1 etc.
     31, 28, 31,
     30, 31, 30,
     31, 31, 30,
     31, 30, 31
 };
+constexpr int DAYS_IN_LEAP_MONTH = 29;
 
-const int LEAP_DAY = 29;
+constexpr int DAYS_IN_YEAR = 365;
+constexpr int DAYS_IN_LEAP_YEAR = 366;
 
+constexpr int HOURS_IN_DAY = 24;
+
+constexpr int MINUTES_IN_HOUR = 60;
+constexpr int MINUTES_IN_DAY = HOURS_IN_DAY * MINUTES_IN_HOUR;
+
+constexpr int SECONDS_IN_MINUTE = 60;
+constexpr int SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
+constexpr int SECONDS_IN_DAY = SECONDS_IN_HOUR * HOURS_IN_DAY;
+// constexpr int SECONDS_IN_28_DAYS = SECONDS_IN_DAY * 28;
+// constexpr int SECONDS_IN_29_DAYS = SECONDS_IN_DAY * 29;
+// constexpr int SECONDS_IN_30_DAYS = SECONDS_IN_DAY * 30;
+// constexpr int SECONDS_IN_31_DAYS = SECONDS_IN_DAY * 31;
+// constexpr int SECONDS_IN_MONTH = SECONDS_IN_DAY * DAYS_IN_MONTH_APPR;
+constexpr int SECONDS_IN_YEAR = SECONDS_IN_DAY * DAYS_IN_YEAR;
+constexpr int SECONDS_IN_LEAP_YEAR = SECONDS_IN_DAY * DAYS_IN_LEAP_YEAR;
+
+constexpr int LEAP_DAY = DAYS_IN_LEAP_MONTH;
+
+namespace DateTime {
 
 struct DateTime {
-    int day{ FIRST_DAY };
-    int month{ FIRST_MONTH };
-    int year{ FIRST_YEAR };
+    int day { FIRST_DAY };
+    int month { FIRST_MONTH };
+    int year { FIRST_YEAR };
 
-    int hour{ FIRST_HOUR };
-    int minute{ FIRST_MINUTE };
-    int second{ FIRST_SECOND };
+    int hour { FIRST_HOUR };
+    int minute { FIRST_MINUTE };
+    int second { FIRST_SECOND };
 
-    inline bool isYearLeap();
+    void getDate() const;
+    void getTime() const;
+    void getDateTime() const;
 
-    void getFull();
-    void getDate();
-    void getTime();
+    // void getNextDay(DateTime &dt);
+    void getNextDay() const;
 
-    void getNextDay(DateTime &dt);
-    void getNextDay();
-
-    void getPrevDay(DateTime &dt);
+    // void getPrevDay(DateTime &dt);
     void getPrevDay();
 
-    void parseDate(const std::string &line);
+    TimeDelta::TimeDelta operator-(const DateTime &datetime2) const;
+
+    DateTime operator+(const TimeDelta::TimeDelta &timedelta) const;
+    DateTime operator-(const TimeDelta::TimeDelta &timedelta);
 };
 
 bool isYearLeap(const int t_year);
 
-bool isDayCorrect(const int t_day, const int t_month, const int t_year);
-bool isMonthCorrect(const int t_month);
-bool isYearCorrect(const int t_year);
+inline bool isDayCorrect(const int t_day, const int t_month, const int t_year);
+inline bool isMonthCorrect(const int t_month);
+inline bool isYearCorrect(const int t_year);
 
 void dayIncorrectError(const int t_day);
 void monthIncorrectError(const int t_month);
 void yearIncorrectError(const int t_year);
 
+void parseDate(const std::string &dateString, DateTime &date);
+
 DateTime systemTime();
+
+long long int dateToSeconds(const DateTime &dt);
+
+void secondsToDate(const long long seconds, DateTime &dt);
+
+} // namespace DateTime
 
 #endif
